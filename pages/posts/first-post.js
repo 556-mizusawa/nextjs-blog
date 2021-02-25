6811b99543c93586ed08e38ce315454e6a057ec1
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-02-25 15:29:26 +0900

    Create the profile page and css file

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,19 @@
 import Link from 'next/link';
+import HEAD from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <HEAD>
+        <title>NextApp</title>
+      </HEAD>
       <h1>FirstPost</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
